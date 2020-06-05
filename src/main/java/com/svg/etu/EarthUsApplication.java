fe8bfffd1b7346d40ed8832e4cdcb6ca49bcbce9
LeeJuWon <dkreka11@gmail.com>
2020-06-05 12:46:12 +0900

    first commit

---
+++ src/main/java/com/svg/etu/EarthUsApplication.java
@@ -0,0 +1,13 @@
+package com.svg.etu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svg/etu/EarthUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svg.etu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
